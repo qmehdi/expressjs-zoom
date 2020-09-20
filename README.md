0880ed8cae3f2e1cbae7042ad3bfce8dbcbf06d0
Qamber Mehdi <qmehdi@Qambers-MBP.attlocal.net>
2020-09-20 16:22:57 -0500

    generate a uuid and redirect to /<uuid>

--- README.md
+++ README.md
@@ -4,4 +4,7 @@ Run
 > npm init
 > npm install express
 > npm install -g nodemon
+> npm install ejs
+
+### To run the application
 > nodemon server.js
--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
+const { v4: uuidv4 } = require('uuid');
 
 app.set('view engine', 'ejs');
 
+// URLs
 app.get('/', (req, res) => {
-    res.render('room');
+    res.redirect(`/${uuidv4()}`); // Generates a uuid and redirects to /<the uuid>
+})
+
+app.get('/:room', (req, res) => {
+    res.render('room', { roomId: req.params.room });    
 })
 
 
