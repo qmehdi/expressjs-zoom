dad09fc5c9027cce09035b2145d5d0839155cc33
Qamber Mehdi <qmehdi110@gmail.com>
2020-09-20 18:53:00 -0500

    Successfully display video

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Run
 > npm init
 > npm install express
 > npm install -g nodemon
-> npm install ejs
+> npm install ejs navigator document getusermedia
 
 ### To run the application
 > nodemon server.js
---
+++ public/script.js
@@ -0,0 +1,25 @@
+// This is where the JavaScript for the Frontend is going to live
+
+// Create a video element and show our own video on the page
+let myVideoStream;
+const videoGrid = document.getElementById('video-grid');
+const myVideo = document.createElement('video');
+myVideo.muted = true;
+
+
+// This is a promise. An event in the future that will be either resolved or rejected
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+}).then(stream => {
+    myVideoStream = stream;
+    addVideoStream(myVideo, stream);
+})
+
+const addVideoStream = (video, stream) => {
+    video.srcObject = stream;
+    video.addEventListener('loadedmetadata', () => {
+        video.play();
+    });
+    videoGrid.append(video);
+}
